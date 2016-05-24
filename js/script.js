f8966176aee38c66511d396cf5200838fc8e0964
Amit Kumar <amit.k03377@gmail.com>
2016-05-24 20:47:51 +1200

    image slider working fine

--- js/script.js
+++ js/script.js
@@ -7,3 +7,24 @@ $('#home-menu').slideToggle();
 
 
 });
+
+// Image slider for main images 
+function slider (){
+	$(".slider #1").show("fade",500);
+	$(".slider #1").delay(3500).hide("slide",{direction:"left"},500);
+
+	var sc = $(".slider img").size();
+	var count = 2;
+
+	setInterval(function(){
+		$('.slider #'+count).show('slide',{direction:'right'},500);
+		$('.slider #'+count).delay(3500).hide('slide',{direction:"left"},500);
+
+		if(count == sc){
+			count = 1;
+		}
+		else{
+			count = count+1;
+		}
+	},4500);
+}
