02912fb57d10d1d9677b37e45c5df7471119ed4d
Amit Kumar <amit.k03377@gmail.com>
2016-05-25 01:55:45 +1200

    slidetoggle working still gotta fix it

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 //listen for when the user clicks on the hamburger menu 
+$(document).ready(function(){
+
+
 $('#menu').on('click', function(){
 
 // if menu is closed , slide down 
@@ -8,16 +11,22 @@ $('#home-menu').slideToggle();
 
 });
 
+
+
+
+
+
+
 // Image slider for main images 
 function slider (){
-	$(".slider #1").show("fade",500);
+	$(".slider #1").show("fade",200);
 	$(".slider #1").delay(3500).hide("slide",{direction:"left"},500);
 
 	var sc = $(".slider img").size();
 	var count = 2;
 
 	setInterval(function(){
-		$('.slider #'+count).show('slide',{direction:'right'},500);
+		$('.slider #'+count).show('slide',{direction:'right'},200);
 		$('.slider #'+count).delay(3500).hide('slide',{direction:"left"},500);
 
 		if(count == sc){
@@ -26,5 +35,36 @@ function slider (){
 		else{
 			count = count+1;
 		}
-	},4500);
+	},4200);
 }
+
+// slide toggle effect on the service div boxes 
+
+$('#service-1-container').hover(function(){
+
+	$('#service-info').slideToggle('slow');
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
