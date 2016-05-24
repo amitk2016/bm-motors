8459a1eb0b25cefe9085dcd5877eca92d74d68f5
Amit Kumar <amit.k03377@gmail.com>
2016-05-24 15:28:53 +1200

    hameburger menu works gotta improve it though

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//listen for when the user clicks on the hamburger menu 
+$('#menu').on('click', function(){
+
+// if menu is closed , slide down 
+// if menu is open, slide down 
+$('#home-menu').slideToggle();
+
+
+});
