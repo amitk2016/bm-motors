d1f2408ca1687693c9f46a9ba17eb5a11411a809
Amit Kumar <amit.k03377@gmail.com>
2016-05-26 02:50:39 +1200

    starting geolocation

--- js/script.js
+++ js/script.js
@@ -17,25 +17,33 @@ $('#home-menu').slideToggle();
 
 
 // Image slider for main images 
+$(function() {
+    $(".rslides").responsiveSlides();
+  });
+
+
+
+
+
 // function slider (){
-	$(".slider #1").show("fade",200);
-	$(".slider #1").delay(3500).hide("slide",{direction:"left"},500);
+// 	$(".slider #1").show("fade",200);
+// 	$(".slider #1").delay(3500).hide("slide",{direction:"left"},500);
 
-	var sc = $(".slider img").size();
-	var count = 2;
+// 	var sc = $(".slider img").size();
+// 	var count = 2;
 
-	setInterval(function(){
-		$('.slider #'+count).show('slide',{direction:'right'},200);
-		$('.slider #'+count).delay(3500).hide('slide',{direction:"left"},500);
+// 	setInterval(function(){
+// 		$('.slider #'+count).show('slide',{direction:'right'},200);
+// 		$('.slider #'+count).delay(3500).hide('slide',{direction:"left"},500);
 
-		if(count == sc){
-			count = 1;
-		}
-		else{
-			count = count+1;
-		}
-	},4200);
-}
+// 		if(count == sc){
+// 			count = 1;
+// 		}
+// 		else{
+// 			count = count+1;
+// 		}
+// 	},4200);
+// }
 
 // slide toggle effect on the service div boxes 
 
